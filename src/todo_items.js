0cf221a359be0663e50bb349d5cb4c815792fce4
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-12-28 16:00:17 -0300

    built module for task creation

---
+++ src/todo_items.js
@@ -0,0 +1,10 @@
+export class Todo {
+    constructor(type, title, description, dueDate, dueTime, priority){
+        this.type = type;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.dueTime = dueTime;
+        this.priority = priority;
+    }
+}
