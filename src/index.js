dc0fbd32f8d0188b260a4d22c1bf1dcc7b2f67fe
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-12-30 13:36:55 -0300

    added logic that creates cards for each todo and displays it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+// imports section
 import { todoNotes, todoTasks, todoSort } from "./todo_sort";
 import { projects } from "./todo_projects";
 import { todoFormOpen, todoFormClose } from "./todo_modal";
 import { todoCreate } from "./todo_create";
+import { Todo, todoList } from "./todo_class";
 
-//
-// Logic for the form pop-up functionality 
-//
+// selectors section
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 const formBtn = document.querySelectorAll('[data-type="formBtn"]');
+const type = document.getElementById('type');
+const dueDate = document.getElementById('dueDate');
+const dueTime = document.getElementById('dueTime');
+const priority = document.getElementById('priority');
+const main = document.querySelector('main');
 
+
+// Logic for the form popup functionality 
 overlay.addEventListener('click', () =>{
     let modals = document.querySelectorAll('.modal.active')
     modals.forEach(modal => {
@@ -30,12 +37,29 @@ closeModalButtons.forEach((element) => {
         todoFormClose() });
 });
 
-//
+// logic to hide/show form options based on todo type
+type.addEventListener('change', () =>{
+    let x = document.getElementById('type');
+    if (x.value === 'Note'){
+        dueDate.style.display = 'none';
+        dueTime.style.display = 'none';
+        priority.style.display = 'none';
+    }else {
+        dueDate.style.display = 'inline';
+        dueTime.style.display = 'inline';
+        priority.style.display = 'inline';
+    }
+})
+
 // Logic for todo creation 
-//
 const submitButton = document.querySelector('[data-submit-button]');
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault()
     todoCreate()
 })
+
+const helloWorld = new Todo('Task', 'Hello World', 'Lorem Ipsum');
+helloWorld.pushToList();
+
+
---
+++ src/todo_card.js
@@ -0,0 +1,53 @@
+import { Todo, todoList } from "./todo_class";
+
+function cardCreate(){
+    const type = document.getElementById('type');
+    const dueDate = document.getElementById('dueDate');
+    const dueTime = document.getElementById('dueTime');
+    const priority = document.getElementById('priority');
+    const main = document.querySelector('main');
+
+    while (main.firstChild) {
+        main.removeChild(main.lastChild)
+    }
+
+    todoList.forEach((element) => {
+        if (element.type === 'Task'){
+            const taskCard = document.createElement('div');
+            taskCard.classList.add('taskCard');
+            
+            const titleH3 = document.createElement('h3');
+            titleH3.innerText = element.title;
+            const descP = document.createElement('p');
+            descP.innerText = element.description;
+            const dateP = document.createElement('p');
+            dateP.innerText = element.dueDate;
+            const timeP = document.createElement('p');
+            timeP.innerText = element.dueTime;
+            const prioriP = document.createElement('p');
+            prioriP.innerText = element.priority;
+    
+            main.appendChild(taskCard);
+            taskCard.appendChild(titleH3);
+            taskCard.appendChild(descP);
+            taskCard.appendChild(dateP);
+            taskCard.appendChild(timeP);
+            taskCard.appendChild(prioriP);
+        } else {
+            const noteCard = document.createElement('div');
+            noteCard.classList.add('noteCard');
+            
+            const titleH3 = document.createElement('h3');
+            titleH3.innerText = element.title;
+            const descP = document.createElement('p');
+            descP.innerText = element.description;
+        
+            main.appendChild(noteCard);
+            noteCard.appendChild(titleH3);
+            noteCard.appendChild(descP);
+        }
+    })
+    
+    }
+
+    export { cardCreate }
